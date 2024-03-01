150c5d62f2b487c3fccd42100dfc5cf44b0a5889
helinaz27 <helinazeleke670@gmail.com>
2024-03-01 23:46:02 +0300

    fix style for header

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='bg-red-300 items-center flex justify-between min-h-24 mb-10'>
+    <div className=' bg-slate-300 items-center flex justify-between min-h-24 '>
         <h1 className='text-black w-full text-center font-bold' >Footer</h1>
        
     
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,16 +4,16 @@ import { useNavigate, NavLink } from 'react-router-dom'
 function Header() {
   const navigate = useNavigate()
   return (
-    <div className='bg-red-300 items-center flex justify-between'>
+    <div className=' bg-slate-100 items-center flex justify-between'>
 
       <NavLink to='/' className="text-center" >
 
-      <h1 className='text-white block mx-auto w-4/5 text-center' > Header</h1>
+      <h1 className='text-black text-xl hover:bg bg-slate-200 block mx-auto w-4/5 text-center' > Header</h1>
 
       </NavLink>
         
         <NavLink to='cart'>
-        <button  className='px-16 h-[45px] bg-blue-800 text-white'>Cart</button>
+        <button  className='px-16 h-[45px] bg-black text-white'>Cart</button>
         </NavLink>
     
     </div>
